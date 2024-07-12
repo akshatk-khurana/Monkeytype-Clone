e985f9b894960828c3a232a9a78b10cf126e706d
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-12 22:25:52 +1000

    Generate words and code user input
    I came up with a cursor system similar to how Monkeytype does theirs, but it is still unimplemented

--- monketype.js
+++ monketype.js
@@ -2,6 +2,7 @@ const textDiv = document.querySelector('#text-div');
 
 document.addEventListener('DOMContentLoaded', () => {
     const words = [];
+    const cursor = [0, 0];
 
     let numberOfWords = 20;
     let request = fetch(`http://127.0.0.1:5000/word-generation-api/${numberOfWords}`, 
@@ -19,15 +20,55 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let i = 0; i < wordsList.length; i++) {
             words.push(wordsList[i]);
         }
+
+        constructTypingDisplay(words);
     })
 
     document.addEventListener('keydown', event => {
         if ('abcdefghijklmnopqrstuvwxyz '.includes(event.key) || event.key == "Space") {
+            let currentElement = getElementBasedOnCursor(cursor);
             let typed = event.key == "Space" ? " " : event.key;
 
+            if (typed === currentElement.innerHTML) {
+                currentElement.style.color = "white";
+            } else {
+                currentElement.style.color = "red";
+                if (currentElement.innerHTML === " ") {
+                    currentElement.innerHTML = typed;
+                }
+            }
+
         } else if (event.key === "Backspace") {
-            
+            // pass
         }
     })
 
 })
+
+function constructTypingDisplay(words) {
+    for (let i = 0; i < words.length; i++) {
+        const wordElement = document.createElement("div");
+        wordElement.className = "word-div";
+
+        let word = words[i];
+        for (let char of word) {
+            const charElement = document.createElement("div");
+            charElement.className = "char-div";
+            charElement.innerHTML = char;
+
+            wordElement.appendChild(charElement);
+        }
+
+        textDiv.appendChild(wordElement);
+    }
+}
+
+function getElementBasedOnCursor(cursor) {
+    const allWordDivs = textDiv.children;
+    const thisWordDiv = allWordDivs[cursor[0]];
+
+    const allCharDivs = thisWordDiv.children;
+    const thisCharDiv = allCharDivs[cursor[1]];
+
+    return thisCharDiv;
+}
