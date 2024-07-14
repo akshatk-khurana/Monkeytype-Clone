e1c57846e2c8ef0dced7ebea43707e07ad85280f
Akshat Khurana <akshat.khurana@yahoo.com>
2024-07-14 13:58:39 +1000

    Worked on typing caret
    I got a physical typing caret to be seen on screen but I am still having trouble dealing with incorrect spaces

--- monketype.js
+++ monketype.js
@@ -1,10 +1,10 @@
 const textDiv = document.querySelector('#text-div');
+const cursor = [0, 0];
+let numberOfWords = 20;
+let numberOfWordsWithSpaces = numberOfWords * 2 - 1;
 
 document.addEventListener('DOMContentLoaded', () => {
     const words = [];
-    const cursor = [0, 0];
-
-    let numberOfWords = 20;
     let request = fetch(`http://127.0.0.1:5000/word-generation-api/${numberOfWords}`, 
         {
             mode: "cors",
@@ -19,33 +19,55 @@ document.addEventListener('DOMContentLoaded', () => {
         let wordsList = response["words"];
         for (let i = 0; i < wordsList.length; i++) {
             words.push(wordsList[i]);
+            if (i != wordsList.length - 1) {
+                words.push(" ");
+            }
         }
-
+        
         constructTypingDisplay(words);
+        updateBorder(cursor, true);
     })
 
     document.addEventListener('keydown', event => {
-        if ('abcdefghijklmnopqrstuvwxyz '.includes(event.key) || event.key == "Space") {
-            let currentElement = getElementBasedOnCursor(cursor);
-            let typed = event.key == "Space" ? " " : event.key;
-
-            if (typed === currentElement.innerHTML) {
-                currentElement.style.color = "white";
-            } else {
-                currentElement.style.color = "red";
-                if (currentElement.innerHTML === " ") {
-                    currentElement.innerHTML = typed;
-                }
-            }
-
-            moveCursor(cursor, numberOfWords);
-
-        } else if (event.key === "Backspace") {
-            // pass
-        }
+        whenKeyPressed(event);
     })
 
 })
+
+function whenKeyPressed(event) {
+    const key = event.key;
+
+    if ('abcdefghijklmnopqrstuvwxyz '.includes(key) || key == "Space") {
+        let currentElement = getElementBasedOnCursor(cursor);
+        let typed = event.key == " " ? "&nbsp;" : event.key;
+
+        if (typed === currentElement.innerHTML) {
+            currentElement.classList.add("correct");
+        } else {
+            currentElement.classList.add("incorrect")
+
+            // If space is pressed wrong
+            console.log(currentElement.parentElement);
+            if (currentElement.innerHTML === "&nbsp;") {
+                console.log("Hello")
+
+                const charElement = document.createElement("div");
+                charElement.className = "char-div incorrect";
+                charElement.innerHTML = typed;
+
+                getElementBasedOnCursor(cursor).parentElement.appendChild(charElement);
+                moveCursor(cursor, numberOfWordsWithSpaces);
+            }
+        }
+
+        updateBorder(cursor, false);
+        moveCursor(cursor, numberOfWordsWithSpaces);
+        updateBorder(cursor, true);
+
+    } else if (event.key === "Backspace") {
+        // pass
+    }
+}
 
 function constructTypingDisplay(words) {
     for (let i = 0; i < words.length; i++) {
@@ -56,13 +78,18 @@ function constructTypingDisplay(words) {
         for (let char of word) {
             const charElement = document.createElement("div");
             charElement.className = "char-div";
-            charElement.innerHTML = char;
+            charElement.innerHTML = char == " " ? "&nbsp;" : char;
 
             wordElement.appendChild(charElement);
         }
 
         textDiv.appendChild(wordElement);
     }
+}
+
+function updateBorder(cursor, on) {
+    const elementToChange = getElementBasedOnCursor(cursor);
+    elementToChange.style.borderLeft = on ? "2px solid #e2b714" : "none";
 }
 
 function getElementBasedOnCursor(cursor) {
